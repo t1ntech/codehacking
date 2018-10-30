e31119dd486b4a9f1adc6f338ea873a2d001a0ca
t1ntech <tino@t1n.nl>
2018-10-30 22:35:03 +0100

    Installed AdminLTE

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,21 @@
+{{-- resources/views/admin/dashboard.blade.php --}}
+
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Profiel Settings</h1>
+@stop
+
+@section('content')
+    <p>Verander hier in de settings van je profiel</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,16 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    dev.code
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://geenstijl.nl">Geenstijl</a>
+
                 </div>
             </div>
         </div>
